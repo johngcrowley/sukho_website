03aaa7b2765cd096ae74532643ed7c6bc1385da6
johngcrowley <johncrowley547@gmail.com>
2021-04-14 15:49:27 -0400

    Initial Deploy

---
+++ app/auth.py
@@ -0,0 +1,82 @@
+from flask import Blueprint, render_template, request, session, url_for, flash, redirect
+from flask_login import login_required, logout_user, current_user, login_user
+from .models import db, User
+from . import login_manager
+from werkzeug.security import generate_password_hash, check_password_hash
+
+# Blueprint Configuration
+auth_bp = Blueprint(
+    'auth_bp', __name__,
+    template_folder='templates',
+    static_folder='static'
+)
+
+# @auth_bp.route('/update/<int:id>',methods=["GET","POST"])
+# def update(id):
+#     name_to_update = User.query.filter_by(User.id == id).first()
+#     if request.method == "POST":
+#         name = request.form.get('name')
+#         password = request.form.getlist('password')
+#         if password == '':
+#             msg = "Passwords must match"
+#             return render_template('signup.html',msg=msg)
+#         name_to_update.name = name
+#         name_to_update.password = password
+#         try:
+#             db.session.commit()
+#             flash("User Updated Successfully")
+#             return render_template("update.html",name_to_update = name_to_update)
+#         except:
+#             flash("Error! Looks like there was a problem. Try again")
+#             return render_template("update.html",name_to_update = name_to_update)
+
+#     else:
+#         return render_template("update.html",name_to_update=name_to_update)
+
+@auth_bp.route('/signup',methods=["GET","POST"])
+def signup():
+    if request.method == "GET":
+        users = User.query.all()
+        return render_template('signup.html',users=users)
+    else:
+        email = request.form.get('email')
+        name = request.form.get('name')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+
+        if user:
+            flash('Email address already exists')
+            return redirect(url_for('auth_bp.signup'))
+        
+        new_user = User(email=email,name=name,password=generate_password_hash(password, method='sha256'))
+        
+        db.session.add(new_user)
+        db.session.commit()
+
+        
+        return redirect(url_for('auth_bp.login'))
+
+@auth_bp.route('/login',methods=['GET','POST'])
+def login():
+    if request.method == "GET":
+        return render_template('login.html')
+    else:
+        email = request.form.get('email')
+        password = request.form.get('password')
+        
+        user = User.query.filter_by(email=email).first()
+
+        if not user or not check_password_hash(user.password, password):
+            flash('Please check your login details and try again.')
+            return redirect(url_for('auth_bp.login'))
+        
+        login_user(user,remember=False)
+        return redirect(url_for('main_bp.index'))
+        
+
+@auth_bp.route('/logout',methods=['GET'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth_bp.login'))
